ab03b4adb6949ba05a563bc56509cd7ea53f41f2
Mason Tay <tay3303@hotmail.com>
2022-07-22 17:46:39 +0700

    Met problem at showing datepicker

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -23,6 +23,10 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+	mux.Get("/generals-quarters", handlers.Repo.Generals)
+	mux.Get("/majors-suite", handlers.Repo.Majors)
+	mux.Get("/search-availability", handlers.Repo.Availability)
+	mux.Get("/contact", handlers.Repo.Contact)
 
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
