f89417b13a88043bbb806acd4bcec93be792a8e3
Mason Tay <tay3303@hotmail.com>
2022-07-22 18:25:33 +0700

    20220722 After changing to GO template

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -27,6 +27,7 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/majors-suite", handlers.Repo.Majors)
 	mux.Get("/search-availability", handlers.Repo.Availability)
 	mux.Get("/contact", handlers.Repo.Contact)
+	mux.Get("/make-reservation", handlers.Repo.Reservation)
 
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
